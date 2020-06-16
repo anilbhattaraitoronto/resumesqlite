ef472c976465e4824d2341dfacf06844c8a025b2
Anil Bhattarai <anil.bhattarai@mail.utoronto.ca>
2020-06-16 13:52:24 -0400

    added skills router and also a trigram for mobile menu button from html symbol &#9776;

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser');
 
+//sqlite database
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./databases/resumedb.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log("Connected to resumedb SQlite database");
+});
+
 //import routes
 
 const indexRouter = require('./routes/index')
@@ -13,6 +22,7 @@ const educationRouter = require('./routes/education')
 const projectsRouter = require('./routes/projects')
 const aboutRouter = require('./routes/about')
 const blogsRouter = require('./routes/blogs')
+const skillsRouter = require('./routes/skils')
 
 //set app parameters
 
@@ -29,6 +39,7 @@ app.use('/education', educationRouter)
 app.use('/projects', projectsRouter)
 app.use('/about', aboutRouter)
 app.use('/blogs', blogsRouter)
+app.use('/skills', skillsRouter)
 
 
 
